0bbe5c9a37d2708d4ecf23ea298751bde771476f
saravanakumarvs23 <saravanakumarvs23@gmail.com>
2021-12-14 18:29:43 +0530

    Sample

--- EmpApp.py
+++ EmpApp.py
@@ -77,7 +77,7 @@ def AddEmp():
     finally:
         cursor.close()
 
-    print("all modification done...")
+    print("all modification done....")
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
